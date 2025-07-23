87217d0cabc41e18cd1e35190669e17a820c821f
Ayush Pandey <8898523+ayushpandey8439@users.noreply.github.com>
2025-07-23 12:34:47 +0200

    Adding logging for requests

---
+++ helpers/Logging.js
@@ -0,0 +1,15 @@
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => `${timestamp} [${level.toUpperCase()}]: ${message}`)
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: './Logs/Logs.log' }) // all updates are stored here
+  ]
+});
+
+module.exports = logger;
